fdff9810e3f2d5c6d9addfa607589a4aedb86411
swebexport <iwan930409@outlook.com>
2021-07-05 03:22:59 +0900

    initial commit

---
+++ src/widgets/Menu/config.ts
@@ -0,0 +1,137 @@
+export const links = [
+  {
+    label: 'Home',
+    icon: 'HomeIcon',
+    href: '/',
+  },
+  {
+    label: 'Trade',
+    icon: 'TradeIcon',
+    items: [
+      {
+        label: 'Exchange',
+        href: 'https://exchange.octopusdefi.com/',
+      },
+      {
+        label: 'Liquidity',
+        href: 'https://exchange.octopusdefi.com/#/pool',
+      },
+    ],
+  },
+  {
+    label: 'Farms',
+    icon: 'FarmIcon',
+    href: '/farms',
+  },
+  {
+    label: 'Staking',
+    icon: 'PoolIcon',
+    href: '/staking',
+  },
+  // {
+  //   label: 'Pools',
+  //   icon: 'PoolIcon',
+  //   href: '/pools',
+  // },
+  {
+    label: 'Lottery',
+    icon: 'TicketIcon',
+    href: '/lottery',
+  },
+  {
+    label: 'Presale',
+    icon: 'PresaleIcon',
+    href: 'https://presale.octopusdefi.com/',
+  },
+  {
+    label: 'Games',
+    icon: 'NftIcon',
+    items: [
+      {
+        label: 'The King of The Forest',
+        href: '/game',
+      },
+    ],
+  },
+  {
+    label: "Info",
+    icon: "InfoIcon",
+    items: [
+      // {
+      //   label: 'Project Info',
+      //   href: '',
+      // },
+      // {
+      //   label: 'Litepaper',
+      //   href: '',
+      // },
+      // {
+      //   label: 'Donation Vault',
+      //   href: '',
+      // },
+      // {
+      //   label: 'Marketing Vault',
+      //   href: '',
+      // },
+    ],
+  },
+  // {
+  //   label: "IFO",
+  //   icon: "IfoIcon",
+  //   items: [
+  //     {
+  //       label: "Next",
+  //       href: "/ifo",
+  //     },
+  //     {
+  //       label: "History",
+  //       href: "/ifo/history",
+  //     },
+  //   ],
+  // },
+  {
+    label: "More",
+    icon: "MoreIcon",
+    items: [
+      // {
+      //   label: "Voting",
+      //   href: "https://voting.pancakeswap.finance",
+      // },
+      {
+        label: 'Github',
+        href: '',
+      },
+      {
+        label: 'Docs',
+        href: '',
+      },
+      {
+        label: 'Medium',
+        href: '',
+      },
+    ],
+  },
+];
+
+export const socials = [
+  {
+    label: "Telegram",
+    icon: "TelegramIcon",
+    href: "https://t.me/toastswaps",
+  },
+  {
+    label: "Medium",
+    icon: "MediumIcon",
+    href: "https://medium.com/@toastswap",
+  },
+  {
+    label: "Twitter",
+    icon: "TwitterIcon",
+    href: "https://twitter.com/SwapToast",
+  },
+];
+
+export const MENU_HEIGHT = 64;
+export const MENU_ENTRY_HEIGHT = 48;
+export const SIDEBAR_WIDTH_FULL = 240;
+export const SIDEBAR_WIDTH_REDUCED = 56;
